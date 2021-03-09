42c84fdbba0536e40ddd09063f62c43910ce9e1d
omkrishna <omkrishna.ok@gmail.com>
2021-03-09 18:47:48 +0530

    queue density

--- SubTask2 1/task2.cpp
+++ SubTask2 1/task2.cpp
@@ -7,7 +7,7 @@
 
 
 #include <iostream>
-#include <sstream>
+#include <fstream>
 
 using namespace cv;
 using namespace std;
@@ -40,6 +40,9 @@ int main(int argc, char** argv)
         return -1;
     }
 
+    Ptr<BackgroundSubtractor> pBackSub = createBackgroundSubtractorMOG2();
+    Mat fgMask;
+
     cvtColor(img,img,COLOR_BGR2GRAY);
 
     vector<Point2f> img_points;
@@ -66,6 +69,9 @@ int main(int argc, char** argv)
     //Displaying output images to the user
     imwrite("Empty.jpg",img_cropped);
 
+    pBackSub->apply(img_cropped, fgMask,0);
+    ofstream MyFile("output.csv");
+
     while (1)
 	{
 		Mat frame;
@@ -79,10 +85,10 @@ int main(int argc, char** argv)
 		vector<Point2f> frame_p;
         vector<Point2f> crframe_p;
 
-        frame_p.push_back(Point2f(314,95));
-        frame_p.push_back(Point2f(435,95));
-        frame_p.push_back(Point2f(529,363));
-        frame_p.push_back(Point2f(187,357));
+        frame_p.push_back(Point2f(968,254));
+        frame_p.push_back(Point2f(1308,260));
+        frame_p.push_back(Point2f(1551,1065));
+        frame_p.push_back(Point2f(587,1065));
 
         crframe_p.push_back(Point2f(0,0));
         crframe_p.push_back(Point2f(400,0));
@@ -92,12 +98,25 @@ int main(int argc, char** argv)
         Mat h = findHomography(frame_p,crframe_p);
         warpPerspective(frame,cropped_frame,h,size);
 
-		cropped_frame = cropped_frame - img_cropped;
+        pBackSub->apply(cropped_frame, fgMask,0);
 
 		s = int2str(c);
 		c++;
-		imwrite("ig" + s + ".jpg", cropped_frame);
+		
+
+        float pixels = fgMask.rows * fgMask.cols;
+        float nonZeroPixels = countNonZero(fgMask);
+        float density = nonZeroPixels/pixels;
+
+        if(c%15==0) {
+            MyFile <<(c/15)<<","<< density<<endl;
+            cout<<(c/15)<<","<< density<<endl;
+            //imwrite("ig" + s + ".jpg", fgMask);
+        }
+
+        
 	}
 
     return 0;
+    MyFile.close();
 }
